4aeaeabf5f6eb68b6e3baa2ee628f98e8b019c0d
Oksana <marjash@i.ua>
2022-09-12 12:13:32 +0300

    delete redundant code

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 @Getter
 @AllArgsConstructor
-public class Point implements Comparator<Point> {
+public class Point {
     private int x;
     private int y;
 
@@ -15,13 +15,5 @@ public class Point implements Comparator<Point> {
                 "x=" + x +
                 ", y=" + y +
                 '}';
-    }
-
-    @Override
-    public int compare(Point p1, Point p2) {
-        int result = p1.getX() - p2.getX();
-        if (result == 0)
-            result = p1.getY() - p2.getY();
-        return result;
     }
 }
